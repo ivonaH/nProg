7e7695200d29ab6694d9c98ca3317eefb5d1de86
IvonaH <ivonaheldrih@gmail.com>
2020-05-02 21:37:44 +0200

    Dodata dokumentacija za DomainObject.

--- mavenCommonLib/src/main/java/rs/ac/bg/fon/ai/nprog/mavenCommonLib/domain/DomainObject.java
+++ mavenCommonLib/src/main/java/rs/ac/bg/fon/ai/nprog/mavenCommonLib/domain/DomainObject.java
@@ -9,18 +9,79 @@ import java.sql.ResultSet;
 import java.util.List;
 
 /**
+ * Interfejs DomainObject predstavlja domenski objekat kog nasledjuju klase u
+ * paketu domain.
  *
  * @author Ivona
+ * 
+ * @version 1.0
  */
 public interface DomainObject {
-    String getTableName();
-    String getParameters();
-    String getParameterNames();
-    int getPrimaryKeyValue();
-    String getPrimaryKeyName();
-    List<DomainObject> convertRSList(ResultSet rs);
-    String getUpdateQuery();
-    String getJoinCondition();
+	/**
+	 * Metoda vraca naziv tabele.
+	 * 
+	 * @return String koji predstavlja naziv tabele.
+	 */
+	String getTableName();
 
-    public String getSortCondition();
+	/**
+	 * Metoda vraca vrednosti za domenski objekat koji zelimo da sacuvamo u tabeli.
+	 * 
+	 * @return String koji predstavlja vrednosti (za dati domenski objekat) koje
+	 *         ubacujemo u tabelu.
+	 */
+	String getParameters();
+
+	/**
+	 ** Metoda vraca nazive kolona za domenski objekat koji zelimo da sacuvamo u
+	 * tabeli.
+	 * 
+	 * @return String koji predstavlja nazive kolona koji ubacujemo u tabelu.
+	 */
+	String getParameterNames();
+
+	/**
+	 * Metoda vraca vrednost primarnog kljuca za dati domenski objekat.
+	 * 
+	 * @return int vrednost primarnog kljuca.
+	 */
+	int getPrimaryKeyValue();
+
+	/**
+	 * Metoda vraca vrednost primarnog kljuca za dati domenski objekat.
+	 * 
+	 * @return String naziv primarnog kljuca.
+	 */
+	String getPrimaryKeyName();
+
+	/**
+	 * Metoda koja cita iz ResultSeta listu domenskih objekata.
+	 * 
+	 * @param rs ResultSet iz kog cemo procitati listu domenskih objekata.
+	 * @return Lista domenskih objekata koji su procitani iz baze.
+	 */
+	List<DomainObject> convertRSList(ResultSet rs);
+
+	/**
+	 * Metoda koja vraca naziv kolone i vrednost na koju ce ta kolona biti
+	 * postavljena.
+	 * 
+	 * @return String naziv kolone i vrednost na koju ce ta kolona biti postavljena.
+	 *         Na primer: " hallId=1"
+	 */
+	String getUpdateQuery();
+
+	/*
+	 * Metoda vraca uslov za spajanje tabele sa 2. tabelom.
+	 * 
+	 * @return String metoda vraca uslov za spajanje tabele sa 2. tabelama.
+	 */
+	String getJoinCondition();
+	/*
+	 * Metoda vraca uslov za sortiranje vrednosti.
+	 * 
+	 * @return String string uslov za sortiranje.
+	 * Na primer: "time asc".
+	 */
+	String getSortCondition();
 }
