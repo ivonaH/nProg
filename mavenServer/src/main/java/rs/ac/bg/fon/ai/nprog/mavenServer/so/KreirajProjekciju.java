2bfb6f7a22007c30001af05c640b798a27f2b475
ivonaH <ivonaheldrih@gmail.com>
2020-06-12 16:18:32 +0200

    Dodat validator za so KreirajProjekciju.

--- mavenServer/src/main/java/rs/ac/bg/fon/ai/nprog/mavenServer/so/KreirajProjekciju.java
+++ mavenServer/src/main/java/rs/ac/bg/fon/ai/nprog/mavenServer/so/KreirajProjekciju.java
@@ -6,18 +6,20 @@
 package rs.ac.bg.fon.ai.nprog.mavenServer.so;
 
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.*;
+import rs.ac.bg.fon.ai.nprog.mavenCommonLib.exception.ValidationException;
+import rs.ac.bg.fon.ai.nprog.mavenServer.validator.showtime.SaveShowtimeValidator;
 
 import java.util.List;
 
 /**
-*
-* @author Ivona
-* 
-* @version 1.0
-* 
-*          Klasa KreirajProjekciju predstavlja sistemsku operaciju koja cuva projekcija u bazi.
-*          Nasledjuje AbstractSystemOperation.
-*/
+ *
+ * @author Ivona
+ * 
+ * @version 1.0
+ * 
+ *          Klasa KreirajProjekciju predstavlja sistemsku operaciju koja cuva
+ *          projekcija u bazi. Nasledjuje AbstractSystemOperation.
+ */
 public class KreirajProjekciju extends AbstractSystemOperation {
 
 	/**
@@ -29,36 +31,52 @@ public class KreirajProjekciju extends AbstractSystemOperation {
 	 * Parametrizovani konstruktor metode kreiraj projekciju.
 	 * 
 	 * @param object koji zelimo da sacuvamo
-	 * @throws Exception ako primljeni objekat nije instanca klase Showtime
+	 * @throws Exception ako primljeni objekat nije instanca klase Showtime ili ako
+	 *                   sala nije dostupna u tom terminu.
 	 */
 	public KreirajProjekciju(Object object) throws Exception {
 		validate(object);
 		this.showtime = (Showtime) object;
+		validateShowtimeDetails();
+
 	}
-	
+
 	/**
 	 * Metoda koja cuva projekciju u bazi.
 	 * 
 	 * @throws Exception ako dodje do greske prilikom cuvanja projekcije u bazi.
 	 */
-    @Override
-    protected void executeSpecificOperation() throws Exception {
-        showtime.setShowtimeId(dbb.generateId(showtime));
-        dbb.saveDomainObject(showtime);
-    }
+	@Override
+	protected void executeSpecificOperation() throws Exception {
+		showtime.setShowtimeId(dbb.generateId(showtime));
+		dbb.saveDomainObject(showtime);
+	}
 
-    /**
- 	 * Metoda koja vrsi validaciju da li je objekat instanca klase
- 	 * Showtime.
- 	 * 
- 	 * @param object Objekat za koji zelimo da proverimo da li je instanca klase
- 	 *               Showtime.
- 	 * @throws Exception ako objekat nije instanca klase Showtime.
- 	 * 
- 	 */
-    protected void validate(Object object) throws Exception {
-        if (!(object instanceof Showtime)) {
-            throw new Exception("Objekat nije instanca klase Projekcija!");
-        }
-    }
+	/**
+	 * Metoda koja vrsi validaciju da li je objekat instanca klase Showtime.
+	 * 
+	 * @param object Objekat za koji zelimo da proverimo da li je instanca klase
+	 *               Showtime.
+	 * @throws Exception ako objekat nije instanca klase Showtime.
+	 * 
+	 */
+	protected void validate(Object object) throws Exception {
+		if (!(object instanceof Showtime)) {
+			throw new Exception("Objekat nije instanca klase Projekcija!");
+		}
+	}
+
+	/**
+	 * Metoda proverava da li je sala slobodna u tom terminu.
+	 * 
+	 * @throws ValidationException ako sala nije slobodna u tom terminu.
+	 */
+
+	protected void validateShowtimeDetails() throws ValidationException {
+		try {
+			SaveShowtimeValidator.validateShowtime(showtime);
+		} catch (Exception ex) {
+			throw new ValidationException("There is already showtime at that time");
+		}
+	}
 }
