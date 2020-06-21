d581de2e3f9529913cb859af75efc67d0a24ed3c
ivonaH <ivonaheldrih@gmail.com>
2020-06-21 14:18:57 +0200

    Dodata 2 testa za neuspesnu izmenu u klasu IzmeniProjekcijuTest.

--- mavenServer/src/test/java/rs/ac/bg/fon/ai/nprog/mavenServer/so/IzmeniProjekcijuTest.java
+++ mavenServer/src/test/java/rs/ac/bg/fon/ai/nprog/mavenServer/so/IzmeniProjekcijuTest.java
@@ -19,11 +19,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.Genre;
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.Hall;
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.Movie;
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.MovieMarathon;
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.Showtime;
 import rs.ac.bg.fon.ai.nprog.mavenCommonLib.domain.User;
+import rs.ac.bg.fon.ai.nprog.mavenCommonLib.exception.ValidationException;
 import rs.ac.bg.fon.ai.nprog.mavenServer.configuration.Configuration;
 import rs.ac.bg.fon.ai.nprog.mavenServer.database.DatabaseConnection;
 
@@ -57,14 +59,20 @@ public class IzmeniProjekcijuTest {
 
 		query = "INSERT INTO movie(MovieId,Name, Genre, Director, Year, DurationInMinutes, UserId) values (1,'Movie1','adventure','Director 1',2020,120,1)";
 		statement.executeUpdate(query);
-		
+
 		query = "INSERT INTO user VALUES(2,'pera','Pera','Peric','pera123')";
 		statement.executeUpdate(query);
 
-		query = "INSERT INTO hall values (1,'Hall1',1)";
+		query = "INSERT INTO hall values (1,'Hall1',10)";
+		statement.executeUpdate(query);
+
+		query = "INSERT INTO hall values (2,'Hall2',2)";
 		statement.executeUpdate(query);
 
 		query = "insert into showtime(ShowtimeId,Date,UserId,HallId,MovieId,time) VALUES(1,'2020-05-15',1,1,1,'15:00:00')";
+		statement.executeUpdate(query);
+
+		query = "insert into showtime(ShowtimeId,Date,UserId,HallId,MovieId,time) VALUES(2,'2020-05-15',1,2,1,'14:00:00')";
 		statement.executeUpdate(query);
 
 		connection.commit();
@@ -104,28 +112,29 @@ public class IzmeniProjekcijuTest {
 		Movie movie = new Movie();
 		izmeniProjekciju = new IzmeniProjekciju(movie);
 	}
-	
+
 	@Test
 	public void testIzmeniProjekcijuUspesnoIzmenjenDatumVremeKorisnik() throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
-		Hall hall=new Hall();
+		Hall hall = new Hall();
 		hall.setHallId(1);
-		Movie movie=new Movie(); 
+		hall.setCapacity(10);
+		Movie movie = new Movie();
 		movie.setMovieId(1);
-		
-		Showtime showtime=new Showtime(sdf.parse("2020-05-16"), stf.parse("16:00:00"), new User(2), hall, movie);
+
+		Showtime showtime = new Showtime(sdf.parse("2020-05-16"), stf.parse("16:00:00"), new User(2), hall, movie);
 		showtime.setShowtimeId(1);
-		
-		 izmeniProjekciju=new IzmeniProjekciju(showtime);
-		 izmeniProjekciju.executeOperation();
-		
-		String querySelect="SELECT * FROM showtime Where showtimeId=1";
+
+		izmeniProjekciju = new IzmeniProjekciju(showtime);
+		izmeniProjekciju.executeOperation();
+
+		String querySelect = "SELECT * FROM showtime Where showtimeId=1";
 		Statement statement = connection.createStatement();
-		ResultSet rs=statement.executeQuery(querySelect);
-		List<Showtime> list=new ArrayList<>();
+		ResultSet rs = statement.executeQuery(querySelect);
+		List<Showtime> list = new ArrayList<>();
 
 		while (rs.next()) {
 			int showtimeId = rs.getInt("ShowtimeId");
@@ -135,40 +144,74 @@ public class IzmeniProjekcijuTest {
 			int hallId = rs.getInt("HallId");
 			int movieId = rs.getInt("MovieId");
 			int movieMarathonId = rs.getInt("MovieMarathonId");
-			Hall h=new Hall();
+			Hall h = new Hall();
 			h.setHallId(hallId);
-			Movie m=new Movie();
+			Movie m = new Movie();
 			m.setMovieId(movieId);
-			list.add(new Showtime(showtimeId, date, time, new User(userId), h,
-					m, new MovieMarathon(movieMarathonId)));
+			list.add(new Showtime(showtimeId, date, time, new User(userId), h, m, new MovieMarathon(movieMarathonId)));
 		}
 
-           assertEquals(list.get(0),showtime);
+		assertEquals(list.get(0), showtime);
 
 	}
-	
-	@Test(expected = SQLException.class)
-	public void testIzmeniProjekcijuNeuspesnoIzmenjenDatum() throws Exception {
-		
+
+	@Test(expected = ValidationException.class)
+	public void testIzmeniProjekcijuNeuspesnoIzmenjenaSalaZbogKapacitetaSale() throws Exception {
+
 		Statement statement = connection.createStatement();
-		String query="INSERT into reservation values (1,'Pera Peric','pera@gmail.com',1,1)";
+		String query = "INSERT into reservation values (1,'Pera Peric','pera@gmail.com',1,1)";
 		statement.executeUpdate(query);
-		
+		query = "INSERT into reservation values (2,'Ana Anic','ana@gmail.com',1,1)";
+		statement.executeUpdate(query);
+		query = "INSERT into reservation values (3,'Jana Janic','ana@gmail.com',1,1)";
+		statement.executeUpdate(query);
+		connection.commit();
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
-		Hall hall=new Hall();
-		hall.setHallId(1);
-		Movie movie=new Movie(); 
+		Hall hall = new Hall();
+		hall.setHallId(2);
+		hall.setCapacity(2);
+		hall.setName("Hall2");
+		Movie movie = new Movie();
 		movie.setMovieId(1);
-		
-		Showtime showtime=new Showtime(sdf.parse("2020-05-20"), stf.parse("15:00:00"), new User(1), hall, movie);
+		movie.setName("Movie1");
+		movie.setGenre(Genre.adventure);
+		movie.setDurationInMinutes(120);
+
+		Showtime showtime = new Showtime(sdf.parse("2020-05-15"), stf.parse("15:00:00"), new User(1), hall, movie);
 		showtime.setShowtimeId(1);
-		
-		izmeniProjekciju=new IzmeniProjekciju(showtime);
+
+		izmeniProjekciju = new IzmeniProjekciju(showtime);
 		izmeniProjekciju.executeOperation();
 
 	}
-	
 
+	@Test(expected = ValidationException.class)
+	public void testIzmeniProjekcijuNespesnoIzmenjenaSalaZauzetaUTomTerminu() throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
+		Hall hall = new Hall();
+		hall.setHallId(2);
+		hall.setCapacity(2);
+		hall.setName("Hall2");
+		Movie movie = new Movie();
+		movie.setMovieId(1);
+		movie.setName("Movie1");
+		movie.setGenre(Genre.adventure);
+		movie.setDurationInMinutes(120);
+
+		Showtime showtime = new Showtime(sdf.parse("2020-05-15"), stf.parse("15:00:00"), new User(1), hall, movie);
+		showtime.setShowtimeId(1);
+
+
+		izmeniProjekciju = new IzmeniProjekciju(showtime);
+		izmeniProjekciju.executeOperation();
+
+		
+
+	}
 }
